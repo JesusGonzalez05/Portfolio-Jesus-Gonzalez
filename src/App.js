ea7fa334ec2d0a2868e9df430112eee79fa6b502
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-06 19:50:40 -0500

    created remaining components.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 // Import the components
 import Header from './components/Header';
 import Footer from './components/Footer';
-import About from './components/About';
+import About from './components/About ';
 import Contact from './components/Contact';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Nav() {
+    // All functional components must have a return method that contains JSX.
+    // We return all the JSX inside a parent element with a className of "container".
+    return (
+      <div className="container">
+        <h1>Hello students!</h1>
+        <p>Today we will be learning about the following:</p>
+        <ul className="parent">
+          <li>React</li>
+          <li>Components</li>
+          <li>Props</li>
+          <li>Babel</li>
+        </ul>
+      </div>
+    );
+  }
+  
