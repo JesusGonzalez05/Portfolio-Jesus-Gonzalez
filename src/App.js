33bb155f0f109437dd8d989e65908a69c5664c64
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-06 19:37:54 -0500

    created app.js - imported components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+
+// Import the components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import Nav from './components/Nav';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
+
+  const [pageIndex, setPageIndex] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
+      <Nav
+        showAbout={() => setPageIndex(0)}
+        showProjects={() => setPageIndex(1)}
+        showContact={() => setPageIndex(2)}
+        showResume={() => setPageIndex(3)}
+        pageIndex={pageIndex}
+      />
+
+      <Header />
+
+      {pageIndex === 0 ? (
+        <About />
+      ) : pageIndex === 1 ? (
+        <Projects />
+      ) : pageIndex === 2 ? (
+        <Contact />
+      ) : pageIndex === 3 ? (
+        <Resume />
+      ) : (
+        <About />
+      )}
+
+      <Footer />
+
     </div>
   );
 }
