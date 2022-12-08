b2aa146b9f7af1665fc9b90eb1345fc17a754cee
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-08 15:28:58 -0500

    created two starter planets for projects.

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,49 +1,29 @@
 import React from 'react';
-// import './Projects.css'
+import './Projects.css'
 
 
 export default function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
-        <div className='solar-system'>
-            <div class="planet">
-            <div class="r1"></div>
-            <div class="r8"></div>
-            <div class="r2"></div>
-            <div class="r3"></div>
-            <div class="r4"></div>
-            <div class="r5"></div>
-            <div class="r6"></div>
-            <div class="r7"></div>
-            <div class="r8"></div>
-            <div class="shad"></div>
+  <div>
+    <h1>Projects</h1>
+
+    <div className='solar-system'>
+      {/* nomdaic adventures */}
+        <div className='container1'>
+
+          <div className='planet-nomadic'>
           </div>
-          <div class="planet">
-            <div class="r1"></div>
-            <div class="r8"></div>
-            <div class="r2"></div>
-            <div class="r3"></div>
-            <div class="r4"></div>
-            <div class="r5"></div>
-            <div class="r6"></div>
-            <div class="r7"></div>
-            <div class="r8"></div>
-            <div class="shad"></div>
+
+        </div>
+
+        {/* CINEMAfiles */}
+        <div className='container2'>
+      
+          <div className='planet-CINEMA'>
           </div>
-          <div class="planet">
-            <div class="r1"></div>
-            <div class="r8"></div>
-            <div class="r2"></div>
-            <div class="r3"></div>
-            <div class="r4"></div>
-            <div class="r5"></div>
-            <div class="r6"></div>
-            <div class="r7"></div>
-            <div class="r8"></div>
-            <div class="shad"></div>
-          </div>
+
         </div>
     </div>
+  </div>
   );
 }
