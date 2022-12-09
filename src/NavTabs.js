e68aafbebce0a063e3ba500f82526bbe4e5e57a3
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-09 08:19:48 -0500

    changed nav layout.

--- src/NavTabs.js
+++ src/NavTabs.js
@@ -8,58 +8,46 @@ import 'bootstrap/dist/css/bootstrap.css';
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-<nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
-    <h1 className="navbar-brand logo">Jesus Gonzalez</h1>
-    <button className="burger">
-      <div className='one'></div>
-      <div className='one'></div>
-      <div className='one'></div>
-    </button>
-    
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-        <button type="button" class="button">   <a id='about' href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
-          About
-        </a></button>
-       
 
-        </li>
-        <li className="nav-item">
-        <button type="button" class="button">  <a id='projects'
-          href="#Projects"
-          onClick={() => handlePageChange('Projects')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-        >
-          Projects
-        </a></button>
-        </li>
-        <li className="nav-item">
-        <button type="button" class="button">   <a id='resume'
-          href="#Resume"
-          onClick={() => handlePageChange('Resume')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a></button>
-        </li>
-        <li className="nav-item">
-        <button type="button" class="button">   <a id='contact'
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a></button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<div>
+<ul class="nav justify-content-center">
+
+  <li class="nav-item">
+    <button type="button" class="button">   
+      <a id='about' href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+            About
+      </a>
+    </button> 
+  </li> 
+
+  <li class="nav-item">
+    <button type="button" class="button">  
+      <a id='projects' href="#Projects" onClick={() => handlePageChange('Projects')} className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>
+      Projects
+      </a>
+    </button>
+  </li>
+
+  <li class="nav-item">
+    <button type="button" class="button">   
+      <a id='resume' href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+      Resume
+      </a>
+      </button>  
+  </li>
+
+  <li class="nav-item">
+    <button type="button" class="button">   
+      <a id='contact' href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+      Contact
+      </a>
+    </button>
+  </li>
+
+</ul>
+</div>
+
+
 
   );
 }
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,18 +5,7 @@ import './Hero.css'
 export default function Hero() {
   return (
     <div class="jumbotron jumbotron-fluid">
-    <div class="planet">
-      <div class="r1"></div>
-      <div class="r8"></div>
-      <div class="r2"></div>
-      <div class="r3"></div>
-      <div class="r4"></div>
-      <div class="r5"></div>
-      <div class="r6"></div>
-      <div class="r7"></div>
-      <div class="r8"></div>
-      <div class="shad"></div>
+
     </div>
-  </div>
   );
 }
