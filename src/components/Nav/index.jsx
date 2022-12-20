0666aa3111e0c239cd2f8d310428c6578e08f4dc
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-20 13:32:20 -0500

    added some styling to nav bar and added link to html for google icons.

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './NavTabs.css'
 import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
 
 
 
@@ -10,15 +11,17 @@ function NavTabs({ currentPage, handlePageChange }) {
   return (
     <div>
     <nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Jesus Gonzalez</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a id='about' href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a></li>
-        <li><a id='projects' href="#Projects" onClick={() => handlePageChange('Projects')} className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>Projects</a></li>
-        <li><a id='resume' href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a></li>
-        <li><a id='contact' href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a></li>
-      </ul>
+    <div class="nav-wrapper black">
+      <div className='container'>
+        <a href="#!" class="brand-logo">Jesus Gonzalez</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a id='about' href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a></li>
+            <li><a id='projects' href="#Projects" onClick={() => handlePageChange('Projects')} className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>Projects</a></li>
+            <li><a id='resume' href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a></li>
+            <li><a id='contact' href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a></li>
+        </ul>
+      </div>
     </div>
   </nav>
 
