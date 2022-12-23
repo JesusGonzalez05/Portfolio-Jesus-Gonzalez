971d89bfec745303f979d53f8eeac87c8c494d57
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-22 21:13:59 -0500

    added margin and height to contact us card.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../App.css'
 
 const Contact = () => {
   return (
     <div>
-      <div class="card w-75 p-3 mx-auto">
+      <div id="contact-card" class=" card w-75 p-3 mx-auto">
         <div class="card-body">
           This is some text within a card body.
         </div>
