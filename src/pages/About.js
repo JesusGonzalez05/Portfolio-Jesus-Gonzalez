d10ba77418eaf3de840006c423eebc81f447e082
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-30 12:46:42 -0500

    finished home page for now and began working on about me page with a boarding pass theme.

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,12 +6,32 @@ import Lottie from 'lottie-react';
 const About = () => {
   return (
     <>
-    <div id="about-container" className="container mt-md-5 mx-md-5 mt-xs-5 mx-xs-5">
-      <div id="about-me" className="card" >
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div id="boarding-container" className="container mt-md-5 mx-md-5 mt-xs-5 mx-xs-5">
+      <div id="boarding-pass" className="card" >
+        <div className="boarding text-center mt-2">
+          <h5 className="card-title">Boarding Pass</h5>
         </div>
+
+          <div className="planets-name">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text"> quick example.</p>
+          </div>
+
+          <div className="">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text"> quick example.</p>
+          </div>
+
+          <div className="about-me">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text"> quick example.</p>
+          </div>
+          <div className="skills">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text"> quick example.</p>
+          </div>
+
+
       </div>
 
     </div>
