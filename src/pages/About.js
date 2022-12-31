223bcf4bb2483f1dd0775b0a644d08e841f4941b
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-31 14:46:10 -0500

    Added an avatar to about me page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import Moon from '../Moon.json';
 import Lottie from 'lottie-react';
-
+import Avatar from '../components/Avatar.png'
 
 const About = () => {
   return (
     <>
     <div id="boarding-container" className="container mt-md-5 mx-md-5 mt-xs-5 mx-xs-5">
-      <div id="boarding-pass" className="card" >
+      <div id="boarding-pass" className="" >
         <div className="boarding text-center mt-2">
           <h5 className="card-title">About Me</h5>
+          <img className="Avatar" src={Avatar} alt="Jesus' avatar"></img>
         </div>
+        
 
-{/* destination name and text */}
-          <h5 className="destination">Desination</h5>
-          <div className="planets-name">
-            <p className="card-text"> quick example.</p>
-          </div>
-
-
-
-          <div className="about-me">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text"> quick example.</p>
-          </div>
-          <div className="skills">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text"> quick example.</p>
-          </div>
 
 
       </div>
