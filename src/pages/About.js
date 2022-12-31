76b3c4210a02c0d6f8e4053f4bc408475dfa0a32
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-31 17:05:00 -0500

    adjusted background and opacity of avatar and about me txt on about page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,15 +5,15 @@ import Avatar from '../components/Avatar.png'
 
 const About = () => {
   return (
-    <>
+    <div className='about-page'>
     <div id="boarding-container" className="container mt-sm-3 mx-sm-5 mt-md-5 mx-md-5">
       <div id="boarding-pass" className="shadow" >
         <div className="title text-center mt-2">
           <p className="card-title">About Me</p>
         </div>
         <img className='Avatar ms-sm-5 mb-sm-5 mb-5 shadow-lg' src = {Avatar} alt="Jesus' avatar"></img>
-        <div className="about-text text-center ">
-          <p>I'm a Software Engineer eager and passionate to create scalable and functional web applications. </p>
+        <div className="about-text text-center shadow-lg ">
+          <p>I'm a Software Engineer eager and passionate in creating scalable and functional web applications. </p>
         </div>
 
 
@@ -23,7 +23,7 @@ const About = () => {
     <div id='lottie-bg'>
     <Lottie animationData={Moon} />
     </div>
-    </>
+    </div>
   );
 }
 export default About;
