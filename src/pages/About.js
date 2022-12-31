5c11780ed00c81b3e6bd9a582507ba1dca60af0f
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-31 18:41:30 -0500

    added devicons for languages and began working on spacing them in about me container.

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,25 +5,40 @@ import Avatar from '../components/Avatar.png'
 
 const About = () => {
   return (
-    <div className='about-page'>
+    <>
     <div id="boarding-container" className="container mt-sm-3 mx-sm-5 mt-md-5 mx-md-5">
       <div id="boarding-pass" className="shadow" >
         <div className="title text-center mt-2">
           <p className="card-title">About Me</p>
         </div>
         <img className='Avatar ms-sm-5 mb-sm-5 mb-5 shadow-lg' src = {Avatar} alt="Jesus' avatar"></img>
-        <div className="about-text text-center shadow-lg ">
-          <p>I'm a Software Engineer eager and passionate in creating scalable and functional web applications. </p>
+        {/* <div className="about-text text-center shadow-lg ">
+          <p>I'm a Software Engineer eager and passionate in create scalable and functional web applications. </p>
         </div>
-
-
+        <div className='icons'>
+          <i id="Devicon" class=" devicon-mongodb-plain-wordmark"></i>
+          <i id="Devicon" class=" devicon-express-original-wordmark"></i>
+          <i id="Devicon" class=" devicon-react-original-wordmark"></i>
+          <i id="Devicon" class=" devicon-nodejs-plain-wordmark"></i>
+        </div> */}
+        <div className='text-icons'>
+          <div className="about-text text-center shadow-lg ">
+            <p>I'm a Software Engineer eager and passionate in create scalable and functional web applications. </p>
+          </div>
+          <div className='icons'>
+            <i id="Devicon" class=" devicon-mongodb-plain-wordmark"></i>
+            <i id="Devicon" class=" devicon-express-original-wordmark"></i>
+            <i id="Devicon" class=" devicon-react-original-wordmark"></i>
+            <i id="Devicon" class=" devicon-nodejs-plain-wordmark"></i>
+          </div>
+        </div>
       </div>
 
     </div>
     <div id='lottie-bg'>
     <Lottie animationData={Moon} />
     </div>
-    </div>
+    </>
   );
 }
 export default About;
