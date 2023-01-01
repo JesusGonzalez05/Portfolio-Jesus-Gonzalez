7cf8d8590838a69fc30903bd510646f7ca7bdca2
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-31 22:02:39 -0500

    added a shadow to languags.

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,7 +16,7 @@ const About = () => {
           <div className="about-text text-center shadow-lg ">
             <p>I'm a Software Engineer eager and passionate in create scalable and functional web applications. </p>
           </div>
-          <div className='icons text-center'>
+          <div className='icons text-center shadow-lg'>
             <i id="Devicon" className="devicon-mongodb-plain-wordmark"></i>
             <i id="Devicon" className="devicon-express-original colored"></i>
             <i id="Devicon" className="devicon-react-original-wordmark"></i>
