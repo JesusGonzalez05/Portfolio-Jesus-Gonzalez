d2df593b7db142b36ca7c1bbd84497991e88afaf
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2023-01-01 17:16:05 -0500

    added another row of prject skeletons.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -24,6 +24,24 @@ export default function Projects() {
     <div className='project-language-container'></div>
   </div>
 
+  <div id="project-container" className="" >
+    <div className='project-preview-container'></div>
+    <div className='project-about-container'></div>
+    <div className='project-language-container'></div>
+  </div>
+
+  <div id="project-container" className="" >
+    <div className='project-preview-container'></div>
+    <div className='project-about-container'></div>
+    <div className='project-language-container'></div>
+  </div>
+
+  <div id="project-container" className="" >
+    <div className='project-preview-container'></div>
+    <div className='project-about-container'></div>
+    <div className='project-language-container'></div>
+  </div>
+
 </div>
   );
 }
