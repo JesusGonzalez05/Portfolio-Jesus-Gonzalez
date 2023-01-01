96b3e422889af7e7f7b2e84efe371844b01597e0
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2023-01-01 16:40:29 -0500

    changed shape and layout of projects containers.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,20 @@
 import React from 'react';
-// import './Projects.css'
+
 
 
 export default function Projects() {
   return (
 <div className='project-page'>
-    <div id="project-title" className="shadow" >
-      <div className="title text-center mt-2">
-        <p className="project-name">Projects</p>
-      </div>
-        <div className='project-img ms-sm-5 mb-sm-5 mb-5 shadow-lg' alt="Jesus' avatar"></div>
-    </div>
+
+  <div id="project-container" className="" >
+  </div>
+
+  <div id="project-container" className="" >
+  </div>
+
+  <div id="project-container" className="" >
+  </div>
+
 </div>
   );
 }
