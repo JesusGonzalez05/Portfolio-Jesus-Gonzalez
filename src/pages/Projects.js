48740ce237fe0ab922ed5948158ff5bde8e4ba22
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2023-01-13 13:04:52 -0500

    added links to deployed sites and repos for each project.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from "react-slick";
+import { Link } from 'react-router-dom';
 
 // project photos
 import Paws1 from '../components/Paws1.png';
@@ -43,9 +44,16 @@ export default function Projects() {
 
       </Slider>
     </div>
+
+    <div className='mt-4 text-center links'>
+      <a target="_blank" rel='Only-Paws' className='btn btn-outline-light' href="https://only-paws.herokuapp.com/">Deployed site</a>
+      <a target="_blank" rel='Only-Paws' className='btn btn-outline-light' href="https://github.com/JesusGonzalez05/only-paws">Repo</a>
+    </div>
+
     <div className='project-about-container is-size-7 p-2 has-text-centered'>
       OnlyPaws is a full-stack web application built using the MERN stack (MongoDB, Express, React, and Node.js) and following the MVC (Model-View-Controller) pattern.
     </div>
+
     <div className='project-language-container p-2'>
             <i id="Devicon" className="devicon-mongodb-plain-wordmark"></i>
             <i id="Devicon" className="devicon-express-original colored"></i>
@@ -62,9 +70,16 @@ export default function Projects() {
         <img src={Cin3} alt="Trulli"></img>
       </Slider>
     </div>
+
+    <div className='mt-4 text-center links'>
+      <a target="_blank" rel='Cinemafiles' className='btn btn-outline-light' href="https://cinemafiles-by-cinemaphiles.herokuapp.com/">Deployed site</a>
+      <a target="_blank" rel='Cinemafiles' className='btn btn-outline-light' href="https://github.com/sangki810/Project-2-The-Cinemaphiles">Repo</a>
+    </div>
+    
     <div className='project-about-container is-size-7 p-2 has-text-centered'>
     CINEMAfiles utilizes Node.js and Express.js to create a RESTful API which is stored in a database using MySQL and the Sequelize ORM. The folder structure meets the MVC paradigm and uses Handlebars.js as the templating engine.
     </div>
+
     <div className='project-language-container p-2'>
     <i id="Devicon" class="devicon-sequelize-plain"></i>
     <i id="Devicon" className="devicon-express-original colored"></i>
@@ -73,11 +88,11 @@ export default function Projects() {
     </div>
   </div>
 
-  <div id="project-container" className="" >
+  {/* <div id="project-container" className="" >
     <div className='project-preview-container'></div>
     <div className='project-about-container'></div>
     <div className='project-language-container'></div>
-  </div>
+  </div> */}
 
 </div>
   );
