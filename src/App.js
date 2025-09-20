4af42574431ff0fe623c966a4d6a207a374675eb
Jesus Gonzalez <Jesusgonzalez051698@gmail.com>
2025-09-20 10:26:33 -0400

    Update App.js
    added some changes to allow routing to display the landing page title as well as any routing issues on load with the hash router and also added an option for a footer.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+// ^ this is should fix the 404 issues on reload.
 
 // import pages
 import Home from './pages/Home';
@@ -9,35 +9,42 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 
-
 // import components
-import Nav from './components/Nav/index';
-// import Hero from './components/Hero/index';
-// import Header from './components/Header/index';
-// import Footer from './components/Footer/index';
-
+import Nav from './components/Nav';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 // import particles 
 import Particles from "./components/particles";
 
-
+// Helper component to render Hero only on the landing page
+function Layout({ children }) {
+  const location = useLocation();
+  return (
+    <>
+      <Particles />
+      <Nav />
+      {location.pathname === "/" && <Hero />}  {/* Only on landing page */}
+      {children}
+      <Footer />
+    </>
+  );
+}
 
 function App() {
-    return (
-      <>
-              <Router>
-                <Particles />
-                <Nav />
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/AboutMe" element={<About />} />
-                  <Route path="/Contact" element={<Contact />} />
-                  <Route path="/Resume" element={<Resume />} />
-                  <Route path="/Projects" element={<Projects />} />
-                </Routes>
-              </Router>
-      </>
-    );
-  }
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutMe" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Resume" element={<Resume />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
 
 export default App;
